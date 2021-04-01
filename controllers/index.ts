f9a34c88c1ee85a71cfe99ea78033ebc9c642d0a
凌 <myywlc@163.com>
2021-04-02 02:02:13 +0800

    init

---
+++ controllers/index.ts
@@ -0,0 +1,24 @@
+import { controller, DarukContext, get, inject, injectable, middleware, Next, prefix } from 'daruk';
+import config from '../config';
+import weather from '../services/weather';
+import utils from '../utils';
+
+@controller()
+@prefix('/comments')
+class Index {
+  @inject('weather') private weather!: weather;
+  @middleware('cors')
+  @get('/')
+  public async index(ctx: DarukContext, next: Next) {
+    // throw new Error('dsa');
+    await this.weather
+      .getWeather()
+      .then((weather: string) => {
+        ctx.body = `Hi, ${config.author}, project version is ${config.version},
+                Today is ${utils.getToday()}, weather is ${weather}`;
+      })
+      .catch((err: string) => {
+        ctx.body = `Get weather information error, message: ${err}`;
+      });
+  }
+}
---
+++ index.ts
@@ -0,0 +1,28 @@
+import { DarukServer, DarukContext, Daruk } from 'daruk';
+import config from './config';
+
+(async () => {
+  let app = DarukServer({
+    name: 'myapp',
+    rootPath: __dirname,
+    // 也可以放404的中间件
+    middlewareOrder: ['koa-ejs', 'koa-favicon', 'errorMid', 'cors']
+  });
+
+  app.on('access', (ctx: DarukContext) => {
+    console.log(ctx.request.id);
+  });
+
+  app.on('exit', (err: Error, daruk: Daruk) => {
+    // maybe you can send a exit error or email
+    daruk.logger.error('exit');
+  });
+
+  await app.loadFile('./glues');
+  await app.loadFile('./services');
+  await app.loadFile('./middlewares');
+  await app.loadFile('./controllers');
+  await app.binding();
+  app.listen(config.port);
+  app.logger.info(`app listen port ${config.port}`);
+})();
