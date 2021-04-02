dc9d12e579a2d7c6757a3f80b209ddbf8002d5f9
jiangchengbiao <jiangchengbiao@aidigger.com>
2021-04-02 17:00:58 +0800

    init

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,7 +1,5 @@
 import { controller, DarukContext, get, inject, injectable, middleware, Next, prefix } from 'daruk';
-import config from '../config';
 import weather from '../services/weather';
-import utils from '../utils';
 
 @controller()
 @prefix('/comments')
@@ -11,15 +9,6 @@ class Index {
   @middleware('cors')
   @get('/')
   public async index(ctx: DarukContext, next: Next) {
-    // throw new Error('dsa');
-    await this.weather
-      .getWeather()
-      .then((weather: string) => {
-        ctx.body = `Hi, ${config.author}, project version is ${config.version},
-                Today is ${utils.getToday()}, weather is ${weather}`;
-      })
-      .catch((err: string) => {
-        ctx.body = `Get weather information error, message: ${err}`;
-      });
+    ctx.body = `Get weather information error, message123`;
   }
 }
